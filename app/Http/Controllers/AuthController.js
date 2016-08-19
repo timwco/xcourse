a30b4569db36a2c7c06f8ca8e0d42362a80309cc
Tim Whitacre <tim@timw.co>
2016-08-18 21:49:07 -0400

    updates to auth controller

--- app/Http/Controllers/AuthController.js
+++ app/Http/Controllers/AuthController.js
@@ -5,6 +5,10 @@ const passport = require('passport');
 class AuthController {
 
 
+  * verify (request, response) {
+    response.json({ authed: true })
+  }
+
   * google (request, response) {
     // Send info to Google
     passport.authenticate('google', { scope: ['profile', 'email'] })(request, response);
@@ -14,16 +18,15 @@ class AuthController {
     // Callback to be run if you can login via Google
     passport.authenticate('google', (err, user, info) => {
       // If any errors...
-      if (err) return res.redirect('/#/admin?c=1');
+      if (err) return response.redirect('/#/admin?c=1');
+
       request.logIn(user, function(err) {
-        if (err) return res.redirect('/#/admin?c=1');
+        if (err) return response.redirect('/#/admin?c=1');
       });
       // Successfully show user is logged in
-      return request.redirect('/#/admin');
+      return response.redirect('/#/admin');
     })(request, response);
   }
-
-
 
 }
 
